6ce5f9551719490e73ae0345a3a1060e7d3fdb06
Elias Jarlebring <jarlebring@gmail.com>
2021-10-27 16:26:45 +0200

    First sp8 evaluation code. work in progress

---
+++ test/test_eval_sp8.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <malloc.h>
+
+#include <cassert>
+#include "sp8_eval.h"
+int main(int argc, char* const  argv[]){
+  int n=2;
+  double *A=(double *)malloc(n*n*sizeof(double));
+  double *mem1=(double *)malloc(n*n*sizeof(double));
+  double *mem2=(double *)malloc(n*n*sizeof(double));
+  double *mem3=(double *)malloc(n*n*sizeof(double));
+  double *mem4=(double *)malloc(n*n*sizeof(double));
+
+  double *b=(double *)malloc(9*sizeof(double));
+  // Matrix: [1 2 ; 3 4];
+  A[0]=1.0;
+  A[1]=2.0;
+  A[2]=3.0;
+  A[3]=4.0;
+
+
+  // polynomial: 1/0!+x*1/1!+x^2*1/2!
+  b[0]=1.0;
+  for (unsigned int ind = 1; ind<9; ind++)
+    b[ind]=b[ind-1]/ind;
+
+  // evaluate it
+  sp8_eval(b,n,A, mem1, mem2, mem3);
+
+
+}
