40d1ac371481bfae05955b0f44a48040888a1c45
Emanuel Rubensson <emanuelrubensson@gmail.com>
2021-10-28 13:03:36 +0200

    Check dimension of array args in python bindings.

--- plots/sp8_acc.py
+++ plots/sp8_acc.py
@@ -8,9 +8,9 @@ get_sp8_params.argtypes = [ctypes.c_double,
                            ctypes.c_double,
                            ctypes.c_int,
                            ctypes.c_int,
-                           ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")]
+                           ndpointer(ctypes.c_double, ndim=1, shape=(9),flags="C_CONTIGUOUS")]
 
 get_sp8_monomial_coefficients = sp8_cwrappers_lib.get_sp8_monomial_coefficients
 get_sp8_monomial_coefficients.restype = None
-get_sp8_monomial_coefficients.argtypes = [ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
-                                          ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")]
+get_sp8_monomial_coefficients.argtypes = [ndpointer(ctypes.c_double, ndim=1, shape=(9), flags="C_CONTIGUOUS"),
+                                          ndpointer(ctypes.c_double, ndim=1, shape=(9), flags="C_CONTIGUOUS")]
