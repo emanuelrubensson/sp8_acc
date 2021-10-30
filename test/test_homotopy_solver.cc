78412b97d9e06ed3b5e910b0ea5977469d2e3eb7
Emanuel Rubensson <emanuelrubensson@gmail.com>
2021-10-30 20:23:30 +0200

    Test flip function.

--- test/test_homotopy_solver.cc
+++ test/test_homotopy_solver.cc
@@ -24,6 +24,17 @@ static void test_homotopy_solver(typename Objective_fun::value_type L_target,
   //    std::cout << std::setprecision(15) << v[ind] << std::endl;
 }
 
+template<typename T>
+static void test_flip() {
+    std::vector<T> v = {3.616269094358620e+04, 2.515360660658102e-01, 3.734847296159297e-02, 1.435436349152357e-01, 3.017655595515090e-01, 4.859232582684261e-01, 6.598969344170539e-01, 8.998903128223554e-01, 9.714353568216227e-01};
+    std::vector<T> v_flipped;
+    get_flipped_polynomial(v, v_flipped);
+    T x = 0.384;
+    T ref = 1-sp8(v,1-x);
+    T value = sp8(v_flipped,x);
+    assert( std::abs(ref-value) < std::sqrt(std::numeric_limits<T>::epsilon()));
+}
+
 int main(int argc, char* const  argv[]){
   double L_target = 0.5;
   double H_target = 0.6;
@@ -38,5 +49,8 @@ int main(int argc, char* const  argv[]){
   L_target = 0.97;
   H_target = 0.98;
   test_homotopy_solver<Objective_Fun70<double> >(L_target, H_target);
+
+  test_flip<double>();
+
   std::cout << argv[0] << " finished OK!" << std::endl;
 }
