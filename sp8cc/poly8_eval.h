ef3a190f38ae43490722ae29916f6b5b2499cb7c
Emanuel Rubensson <emanuelrubensson@gmail.com>
2024-06-25 15:57:40 +0200

    New scheme for evaluation of eight order polynomial, replaces previous one. Not requiring negating if b8<0.

--- sp8cc/poly8_eval.h
+++ sp8cc/poly8_eval.h
@@ -14,67 +14,56 @@ namespace sp8 {
    */
   template<typename T_matrix>
     void poly_8_eval(std::vector<typename T_matrix::value_type> const & mc,
-		     T_matrix & A,
-		     T_matrix & A2,
-		     T_matrix & M2,
-		     T_matrix & M3) {    
+		     T_matrix & X,
+		     T_matrix & X2,
+		     T_matrix & T1,
+		     T_matrix & T2) {
     typedef typename T_matrix::value_type scalar_type;
     const scalar_type b0=mc[8], b1=mc[7], b2=mc[6];
     const scalar_type b3=mc[5], b4=mc[4], b5=mc[3];
     const scalar_type b6=mc[2], b7=mc[1], b8=mc[0];
-    if (b8<0) {
-      // Avoid complex numbers by switching evaluating with minus sign and
-      // subsequently reversing
-      std::vector<scalar_type> mc_minus(9);
-      for (unsigned int ind = 0; ind<9; ind++)      
-	mc_minus[ind]=-mc[ind];
-      poly_8_eval(mc_minus, A, A2, M2, M3);
-      A.negate();
-      return;
-    }
-    // plus minus std::sqrt(b8): Two options possible, both should work fine!
-    const scalar_type c4  = std::sqrt(b8);
-    const scalar_type c3  = b7 / (2.0 * c4);
-    const scalar_type de2 = (b6 - c3*c3) / c4;
-    const scalar_type de1 = (b5 - c3 * de2) / c4;
+    const scalar_type f4 = b8;
+    const scalar_type c1 = b7 / (2.0 * f4);
+    const scalar_type t2 = b6/f4 - c1*c1;
+    const scalar_type t1 = b5/f4 - c1*t2;
     // Extra degree of freedom d0 added to avoid complex coefficients
-    // d0 chosen to make [expression under sqrt] = 1 (this should always
-    // be possible if b8 != 0, which we have assumed anyway). Other
-    // positive choices may also be fine. However, [expression under
-    // sqrt] = 0 makes scheme break if de2 is near 0.
-    const scalar_type d0 = (1-de2*de2+4*b4-4*c3*de1)/(4*c4);
-    // plus minus : Two options possible, choosing + --> e2-d2 = 1
-    // which is good due to later division by e2-d2.
-    const scalar_type e2 = 0.5*(de2+1);
-    const scalar_type d2 = 0.5*(de2-1);
-    const scalar_type e1 = (c3*d0+de1*e2-b3); // /(e2-d2) (which is = 1)
-    const scalar_type d1 = de1-e1;
-    const scalar_type f2 = b2-(d0*e2+d1*e1);
-    const scalar_type f1 = b1-d0*e1;
+    // d0 chosen to make [expression under sqrt] = 1 (this should
+    // always be possible if b8 != 0, which we have assumed
+    // anyway). Other positive choices may also be fine, but our
+    // choice leads to e2-d2=1.
+    const scalar_type d0 = (1.0-t2*t2+4*b4/f4-4*c1*t1)/4.0;
+    // plus minus : Two options possible for e2, choosing +
+    // --> e2-d2=1 which is good due to later division by e2-d2.
+    const scalar_type e2 = 0.5*(t2+1);
+    const scalar_type d2 = 0.5*(t2-1);
+    const scalar_type e1 = c1*d0+t1*e2-b3/f4; // /(e2-d2) (which is = 1)
+    const scalar_type d1 = t1-e1;
+    const scalar_type f2 = b2-f4*(d0*e2+d1*e1);
+    const scalar_type f1 = b1-f4*d0*e1;
     const scalar_type f0 = b0;
-    const scalar_type one = 1.0;
-    M2 = A2;                                    // M2 = A2
-    M2.scale_and_add(c4, c3, A);                // M2 = c4*M2 + c3*A
-    M3.multiply(A2,M2);                         // M3 = A2*M2
-    M2 = M3;                                    // M2 = M3
-    M2.scale_and_add(one, d2, A2);              // M2 = M2 + d2*A2
-    M2.scale_and_add(one, d1, A);               // M2 = M2 + d1*A    
-    M2.add_scaled_identity(d0);                 // M2 = M2 + d0*I
-    M3.scale_and_add(one, e1, A);               // M3 = M3 + e1*A
-    A.scale_and_add(f1, f2, A2);                // A = f1*A + f2*A2
-    A.add_scaled_identity(f0);                  // A = A+f0*I
-    A2.scale_and_add(e2, one, M3);              // A2 = e2*A2 + M3
-    M3.multiply(M2,A2);                         // M3 = M2*A2
-    A.scale_and_add(one, one, M3);              // A = A + M3       
+    //    const scalar_type one = 1.0;
+    T1 = X2;                                    // T1 = X2
+    T1.scale_and_add(1.0, c1, X);               // T1 = T1 + c1*X
+    T2.multiply(X2,T1);                         // T2 = X2*T1
+    T1 = T2;                                    // T1 = T2
+    T1.scale_and_add(1.0, d2, X2);              // T1 = T1 + d2*X2
+    T1.scale_and_add(1.0, d1, X);               // T1 = T1 + d1*X
+    T1.add_scaled_identity(d0);                 // T1 = T1 + d0*I
+    T2.scale_and_add(1.0, e1, X);               // T2 = T2 + e1*X
+    X.scale_and_add(f1, f2, X2);                // X = f1*X + f2*X2
+    X.add_scaled_identity(f0);                  // X = X+f0*I
+    X2.scale_and_add(e2, 1.0, T2);              // X2 = e2*X2 + T2
+    T2.multiply(T1,X2);                         // T2 = T1*X2
+    X.scale_and_add(1.0, f4, T2);               // X = X + f4*T2
   }    
 
   template<typename T_matrix>
     void poly_8_eval(std::vector<typename T_matrix::value_type> const & mc,
-		     T_matrix & A,
-		     T_matrix & A2) {
-    T_matrix M2;
-    T_matrix M3;
-    poly_8_eval(mc, A, A2, M2, M3);
+		     T_matrix & X,
+		     T_matrix & X2) {
+    T_matrix T1;
+    T_matrix T2;
+    poly_8_eval(mc, X, X2, T1, T2);
   }
 } // end namespace sp8
 #endif
